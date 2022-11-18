29de148f37e46ce37770aa68fbe29e2d31296b49
Kishan Kumar Maurya <kishankumarmaurya@CFs-MacBook-Pro-6.local>
2022-11-18 16:05:28 +0530

    Cashfree Prepiad Card Sample App

--- App.tsx
+++ App.tsx
@@ -1,20 +1,88 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Button, View } from 'react-native';
+import { CFCardFlow, CfResetPinFlow } from 'react-native-cashfree-prepaid-cards';
 
-export default function App() {
+const HomeScreen = () => {
+  const [resetPinOpen, setResetPinOpen] = useState(false);
+
+  /**
+   * This function is to capture card detail callback events
+   * @param event 
+   */
+  const cardEvent = (event: any) => {
+    console.log('CF Prepaid Card event', event);
+  };
+
+  /**
+   * 
+   * @param event This function is to capture reset pin callbacks events
+   */
+  const resetPinEvent = ( event : any) =>{
+    console.log('CF Reset Pin event', event);
+    switch (event) {
+      case 'CF_RESET_PIN_SCREEN_DISMISS':
+        setResetPinOpen(!resetPinOpen);
+        break;
+      default:
+        break;
+    }
+  }
+
+  /**
+   * Get this token from App's backend.
+   */
+  const token =
+    'eyJzZGtWZXJzaW9uIjoiMC4wLjEiLCJvcmdJZCI6IjIyMDMyODEzMTUyMTM1M0lEMU9JRDc0MDgyNTIiLCJwaG9uZSI6Ijk4NzA4OTY5NjYiLCJzZGtMb2dpblRva2VuIjoiQzkxQ0hhN1ZjWWZVRjEwWTFOeWFMYUk4dFNtenJqa3hPbjErMVd3eXNOd2hZaVR3PSIsInByb3ZpZGVyQ2FyZEhvbGRlcklkIjoiMjIxMTAzMDk0ODUyMjI5SUQxQ1VTVElENDYzMjMzNCIsInByZXBhaWRDYXJkcyI6W3sicHJvdmlkZXJDYXJkSWQiOiIyMjExMDMwOTQ4NTIyODNJRDFDQVJEODU0MDM1MiIsIm5hbWVPbkNhcmQiOiJ0ZXN0IiwibGFzdDRkaWdpdHMiOiI5MzE2In1dfQ==';
+  
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.cardContainer}>
+        <CFCardFlow
+          environment="sanbox"
+          templateId="default"
+          token={token}
+          onEvent={cardEvent}
+        />
+      </View>
+      <View style={styles.buttonStyle}>
+        <Button
+          onPress={() => {
+            setResetPinOpen(!resetPinOpen);
+          }}
+          title="Reset Pin Flow"
+        />
+        {resetPinOpen ? (
+          <CfResetPinFlow
+            resetPinContainerStyle={styles.pinContainerStyle}
+            onEvent={resetPinEvent}
+          />
+        ) : (
+          <></>
+        )}
+      </View>
     </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  container: { 
+    flex: 1, 
+    padding: 16, 
+    marginHorizontal: 8, 
+    marginVertical: 16 
+  },
+  cardContainer: {
+    marginTop: 16,
+    height: 300,
+  },
+  pinContainerStyle: {
+    height: '50%',
+    marginTop: 'auto',
+  },
+  buttonStyle: {
+    marginTop: 8,
   },
 });
+
+
+export default HomeScreen;
---
+++ README.md
@@ -0,0 +1,21 @@
+# react-native-cashfree-prepaidcard-sample
+
+This is a sample react-native application that consumes the cashfree prepaid card sdk for card details and card pin reset flow.
+
+To clone use this link https://github.com/cashfree/react-native-cashfree-prepaid-card.git
+
+```
+git clone https://github.com/cashfree/react-native-cashfree-prepaid-card.git
+cd react-native-cashfree-prepaid-card
+npm install or yarn install
+npm run start or yarn run start
+```
+
+
+<img src="https://user-images.githubusercontent.com/118521093/202695691-c8769472-dc44-4f85-953b-eb99949d8faf.png" width="200" height="350" />
+
+<img src="https://user-images.githubusercontent.com/118521093/202695706-a0056847-693f-44a0-bfcb-1f2331456234.png" width="200" height="350" />
+
+
+
+https://user-images.githubusercontent.com/118521093/202693386-18974546-bdf0-44c9-a1a2-a6d5547d0e5b.mp4
